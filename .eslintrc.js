8c4795eca0b062f46ebfa28207cefc46d6d46315
Tema <shutemov1997@mail.ru>
2022-09-14 22:18:25 +0300

    chore: linter fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-recommended", "@vue/typescript/recommended"],
   parserOptions: {
     ecmaVersion: 2020,
   },
