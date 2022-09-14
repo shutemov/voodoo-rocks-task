25ef4d66816d668cdeba362b55eb67b290c9a6ba
Tema <shutemov1997@mail.ru>
2022-09-14 22:03:38 +0300

    feat(TPosts): defines variables

--- src/components/TPosts.vue
+++ src/components/TPosts.vue
@@ -6,6 +6,11 @@ import jsonplaceholdersService from "@/services/jsonplaceholderService";
 import { TPost } from "@/types/types";
 import MPagination from "@/components/MPagination.vue";
 import MSearchInput from "@/components/MSearchInput.vue";
+
+const posts: Ref<TPost[]> = ref([]);
+const query = ref("");
+const currentPage = ref(0);
+const limit = ref(20);
 </script>
 <style lang="scss" scoped>
 </style>
