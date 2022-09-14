9d41c8a40e4a8bc9e8df3d8b0775be60129b3403
Tema <shutemov1997@mail.ru>
2022-09-14 21:59:57 +0300

    feat: creates pagination molecule

---
+++ src/components/MPagination.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    class="btn btn-outline-primary"
+    @click="$emit('click:decr', currentPage)"
+  >
+    ◀️
+  </button>
+  <span class="mr-2 ml-2">
+    {{ currentPage }}
+  </span>
+  <button
+    class="btn btn-outline-primary"
+    @click="$emit('click:incr', currentPage)"
+  >
+    ▶️
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  currentPage: { type: Number },
+});
+
+defineEmits(["click:incr", "click:decr"]);
+</script>
+
+<style></style>
