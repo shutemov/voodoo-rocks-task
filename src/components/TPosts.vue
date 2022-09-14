925df95a498da608011739ca78b0dbfd0adcdca7
Tema <shutemov1997@mail.ru>
2022-09-14 22:03:00 +0300

    feat(TPosts): adds imports

--- src/components/TPosts.vue
+++ src/components/TPosts.vue
@@ -1,6 +1,11 @@
 <template>
 </template>
 <script setup lang="ts">
+import { ref, onMounted, Ref, computed } from "vue";
+import jsonplaceholdersService from "@/services/jsonplaceholderService";
+import { TPost } from "@/types/types";
+import MPagination from "@/components/MPagination.vue";
+import MSearchInput from "@/components/MSearchInput.vue";
 </script>
 <style lang="scss" scoped>
 </style>
